ab70acae1fe290f5374de4256e7e092aafa4ec09
Joseph Tubridy and Trevor Teigen <joseph@>
2019-06-06 16:49:21 -0700

    added input error

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,49 @@
 $(function(){
 
+//Backend Logic
 var color;
 var food;
 var pet;
+var total;
+var name;
+var calc = function (color,food,pet) {
+  return color+food+pet;
+}
+
+
+
+
+
+
+
 
 $("#form1").submit(function(event){
-    color = $("input:radio[name=color]:checked").val();
+    name = $("input#name1").val();
+
+    color = parseInt($("input:radio[name=color]:checked").val());
     event.preventDefault();
-    food = $("input:radio[name=food]:checked").val();
-    event.preventDefault();
-    pet = $("input:radio[name=animal]:checked").val();
-    event.preventDefault();
-})
+    food = parseInt($("input:radio[name=food]:checked").val());
+    pet = parseInt($("input:radio[name=animal]:checked").val());
+
+    total = calc(color,food,pet);
+
+      if (name.length % 2 ==0) {
+      // if () {
+        $(".personB1").text(total)
+      } else if (name.length % 2 ==1){
+        $(".personB2").text(total)
+      } else {
+
+      }
+
 
 })
 
 
-// input -> var
+
+
+
+})
 
 
 
